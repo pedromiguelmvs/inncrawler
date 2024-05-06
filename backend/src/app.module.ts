efca9503e1d1ca1b1e88d98de30a7aa615d641b4
miguelito ツ <pedromiguelmvs>
2024-05-06 20:34:04 -0300

    feat(handlers): global handler

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScraperModule } from './modules/scraper/scraper.module';
 import { RoomsModule } from './modules/rooms/rooms.module';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './common/handlers/global-exception.filter';
 
 @Module({
   imports: [ConfigModule.forRoot(), ScraperModule, RoomsModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
